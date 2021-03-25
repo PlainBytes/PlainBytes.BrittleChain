bd108c8432cd075c0c315625cc2d1ea25bfa66ef
Árpád Barta <arpad.barta@outlook.com>
2021-03-25 20:34:08 +0200

    Adds docs to Maybe.cs

--- source/PlainBytes.BrittleChain/Maybe.cs
+++ source/PlainBytes.BrittleChain/Maybe.cs
@@ -2,12 +2,25 @@
 
 namespace PlainBytes.BrittleChain
 {
+    /// <summary>
+    /// Simple value object, allows transmission of value or exception.
+    /// </summary>
+    /// <typeparam name="T">Type of wrapped value.</typeparam>
     public class Maybe<T>
     {
+        /// <summary>
+        /// Stores the wrapped value.
+        /// </summary>
         public T Value { get; }
 
+        /// <summary>
+        /// Stores the wrapped exception.
+        /// </summary>
         public Exception Exception { get; }
 
+        /// <summary>
+        /// Indicates if the <see cref="Value"/> was set, true if it was otherwise false.
+        /// </summary>
         public bool HasValue { get; }
 	
         internal Maybe(T value)
@@ -21,6 +34,10 @@ namespace PlainBytes.BrittleChain
             Exception = exception;
         }
 
+        /// <summary>
+        /// <inheritdoc cref="object.ToString"/>
+        /// </summary>
+        /// <returns>If <see cref="Value"/> was set returns its <see cref="object.ToString()"/> otherwise the <see cref="Exception"/>s message.</returns>
         public override string ToString()
         {
             return HasValue ? Value.ToString() : Exception.Message;
