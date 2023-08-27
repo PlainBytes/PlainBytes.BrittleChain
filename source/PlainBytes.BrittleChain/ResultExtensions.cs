a9929bde1b2952219afcd306b360bdd119e833f6
Árpád Barta <arpad.barta@outlook.com>
2023-08-27 17:51:13 +0300

    Adds ResultTests.cs and ResultExtensionsTests.cs

--- source/PlainBytes.BrittleChain/ResultExtensions.cs
+++ source/PlainBytes.BrittleChain/ResultExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace PlainBytes.BrittleChain
 {
@@ -13,14 +14,22 @@ namespace PlainBytes.BrittleChain
         /// <param name="value">Value which will be contained</param>
         /// <typeparam name="T">Type of provided value</typeparam>
         /// <returns>Instance of  <see cref="Result{T}"/> holding the provided value.</returns>
-        public static Result<T> ToResult<T>(this T value) => Result<T>.CreteFrom(value);
-        
+        public static Result<T> ToResult<T>(this T value)
+        {
+            if (value is Exception exception)
+            {
+                return Result<T>.CreateFromException(exception);
+            }
+            
+            return Result<T>.CreateFrom(value);
+        }
+
         /// <summary>
         /// Wraps the provided value into a <see cref="Result{T}"/> than a <see cref="value"/>.
         /// </summary>
         /// <param name="value">Value which will be contained</param>
         /// <typeparam name="T">Type of provided value</typeparam>
         /// <returns>Instance of  <see cref="Task{TResult}"/> holding the provided value.</returns>
-        public static Task<Result<T>> ToResultTask<T>(this T value) => Task.FromResult(Result<T>.CreteFrom(value));
+        public static Task<Result<T>> ToResultTask<T>(this T value) => Task.FromResult(value.ToResult<T>());
     }
 }
---
+++ tests/PlainBytes.BrittleChain.Tests/ResultExtensionsTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace PlainBytes.BrittleChain.Tests
+{
+    public class ResultExtensionsTests
+    {
+        [Fact]
+        public void ToResult_GivenValue_ThanSucceed()
+        {
+            // Act
+            var result = "test".ToResult();
+
+            // Assert
+            result.Exception.Should().BeNull();
+            result.Value.Should().Be("test");
+            result.Succeeded.Should().BeTrue();
+            result.Failed.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ToResult_GivenException_ThanFail()
+        {
+            // Act
+            var result = new Exception().ToResult();
+
+            // Assert
+            result.Exception.Should().NotBeNull();
+            result.Value.Should().BeNull();
+            result.Succeeded.Should().BeFalse();
+            result.Failed.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ToResultResult_GivenResult_ThanSucceed()
+        {
+            // Act
+            var result = "test".ToResultTask().Result;
+
+            // Assert
+            result.Exception.Should().BeNull();
+            result.Value.Should().Be("test");
+            result.Succeeded.Should().BeTrue();
+            result.Failed.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ToResultResult_GivenException_ThanFail()
+        {
+            // Act
+            var result = new Exception().ToResultTask().Result;
+
+            // Assert
+            result.Exception.Should().NotBeNull();
+            result.Value.Should().BeNull();
+            result.Succeeded.Should().BeFalse();
+            result.Failed.Should().BeTrue();
+        }
+    }
+}
